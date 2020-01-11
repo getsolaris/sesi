ced865d5c7cf487b7a7584b093aec0420bc6f17e
Mingeun Kim <mingeun.k.k@gmail.com>
2020-01-11 18:15:55 +0900

    Added crawl rss version contents

--- version_crawler.py
+++ version_crawler.py
@@ -1,15 +1,30 @@
 import re
 import feedparser
 
-VERSIONS = []
 RSS_URL = 'http://blog.rss.naver.com/saskinio'
 
 def release_rss_crawl(version):
+    versions = []
     content = feedparser.parse(RSS_URL)
 
     for feed in content.entries:
         if feed.category == '릴리즈':
-            VERSIONS.append([re.sub('[^0-9.]', '', feed.title).strip(), feed.link])
+            versions.append([re.sub('[^0-9.]', '', feed.title).strip(), feed.link])
             break
 
-    return [version == VERSIONS[0][0], VERSIONS]
+    return [version == versions[0][0], versions]
+
+def version_content():
+    releases = []
+    content = feedparser.parse(RSS_URL)
+
+    for feed in content.entries:
+        if feed.category == '릴리즈':
+            summary = feed.summary.replace('*', '\n')
+
+            releases.append([
+                re.sub('[^0-9.]', '', feed.title).strip(),
+                summary
+            ])
+
+    return releases
